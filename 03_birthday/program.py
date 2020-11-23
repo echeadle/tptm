b80dcc84ed4d8c55feb89b7c73d9bf40a58b9cf9
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-22 21:56:20 -0700

    redid the compute function

--- 03_birthday/program.py
+++ 03_birthday/program.py
@@ -16,8 +16,10 @@ def get_birthday_from_user():
     return birthday
 
 
-def compute_days_between_dates():
-    pass
+def compute_days_between_dates(original_date, target_date):
+    this_year = datetime.date(target_date.year, original_date.month, original_date.day)
+    dt = this_year - target_date
+    return dt.days
 
 
 def print_birthday_information():
@@ -27,9 +29,10 @@ def print_birthday_information():
 def main():
     print_header()
     bday = get_birthday_from_user()
-    now = None
-    number_of_days = compute_days_between_dates(bday)
-    print_birthday_information(number_of_days)
+    today = datetime.date.today()
+    number_of_days = compute_days_between_dates(bday, today)
+    print(number_of_days)
+    #print_birthday_information(number_of_days)
 
 
 main()
