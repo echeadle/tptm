2e9bd12e5125071088addea4f0a01865e8542bc4
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-23 15:50:22 -0700

    initial commit of journal app

---
+++ 04_journal/program.py
@@ -0,0 +1,16 @@
+def print_header():
+    print('-' * 40)
+    print('          JOURNAL APP')
+    print('-' * 40)
+    print()
+
+
+def run_event_loop():
+    pass
+
+
+def main():
+    print_header()
+
+
+main()
