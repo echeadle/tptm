78b9fd697981de1e8fde48d4736d5d26623ee0a0
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-23 16:15:44 -0700

    Added event_loop

--- 04_journal/program.py
+++ 04_journal/program.py
@@ -1,16 +1,29 @@
 def print_header():
     print('-' * 40)
-    print('          JOURNAL APP')
+    print('           JOURNAL APP')
     print('-' * 40)
     print()
 
 
 def run_event_loop():
-    pass
+    print('What do you want to do with your journal?')
+    cmd = None
+
+    while cmd != 'x':
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+
+        if cmd == 'L':
+            print('L')
+
+        elif cmd == 'A':
+            print('A')
+
+    print('Done, goodbye.')
 
 
 def main():
     print_header()
+    run_event_loop()
 
 
 main()
