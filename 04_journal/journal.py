d6ed2f966c2fbe6ff6239054114a18aebfe6e5b0
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-25 12:01:01 -0700

    Added load function

--- 04_journal/journal.py
+++ 04_journal/journal.py
@@ -2,18 +2,31 @@ import os
 
 def load(name):
     # todo: populate from file if it exists.
-    return []
+    data = []
+    filename = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename, 'r') as fin:
+            for entry in fin.readlines():
+                print("would load: " + entry.rstrip())
+
+    return data
 
 def save(name, journal_data):
-    filename = os.path.abspath(os.path.join('./journals/', name + '.jrl'))
+    filename = get_full_pathname(name)
     print(f'..... saving to: {filename}')
 
     #fout = open(filename, 'w')
-    with open(filename, 'w'):
+    with open(filename, 'w') as fout:
         for entry in journal_data:
             fout.write(entry + '\n')
         
     #fout.close()
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('./journals/', name + '.jrl'))
+    return filename
+
 
 def add_entry(text,journal_data):
     journal_data.append(text)
