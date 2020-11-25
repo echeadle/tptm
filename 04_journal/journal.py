29c2c4cce414ebb234afc6a6b3868e32ede05dcb
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-24 20:49:39 -0700

    Added  adding full path

--- 04_journal/journal.py
+++ 04_journal/journal.py
@@ -1,9 +1,14 @@
+import os
+
 def load(name):
     # todo: populate from file if it exists.
     return []
 
 def save(name, journal_data):
-    pass
+    filename = os.path.abspath(os.path.join('./journals/', name + '.jrl'))
+    print(f'would load from: {filename}')
+    #fout = open(filename, 'w')
+
 
 def add_entry(text,journal_data):
     journal_data.append(text)
