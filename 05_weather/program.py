888165cde268a724a4cbac6d708d037cfa20a268
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-26 14:04:34 -0700

    Added header

--- 05_weather/program.py
+++ 05_weather/program.py
@@ -1,8 +1,19 @@
+def main():
+    # Show the header
+    show_header()
+    # Get the location request
+    location_text 
+    # Convert plaintext over to data we can use
+    # Get report from the API.
+    # Report the weather
+    print("Hello from weather main!")
 
 
-# Show the header
-# Get the location request
-# Convert plaintext over to data we can use
-# Get report from the API.
-# Report the weather
-# 
+def show_header():
+    print('-' * 40)
+    print('           WEATHER CLIENT')
+    print('-' * 40)
+    print()
+
+if __name__ == '__main__':
+    main()
