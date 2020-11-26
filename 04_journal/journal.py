eedacf0b3ce02dd270111e35aa030150f2918d28
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-26 12:33:39 -0700

    Added docstrings to all journal methods and the one for the file.

--- 04_journal/journal.py
+++ 04_journal/journal.py
@@ -1,9 +1,14 @@
+"""
+This modules implements the Journal.
+"""
+
 import os
+
 
 def load(name):
     """
-    This method creates and loads a new journa.
-    :param name: Tis is the base name of the journal to load.
+    This method creates and loads a new journal.
+    :param name: This is the base name of the journal to load.
     :return: A new joural data structure populated with the file data.
     """
     data = []
@@ -17,6 +22,11 @@ def load(name):
     return data
 
 def save(name, journal_data):
+    """
+    This method saves the journal to disk
+    param name: This is the base name of the new journal.
+    param journal_data: This is the list of entries to save.
+    """
     filename = get_full_pathname(name)
     print(f'..... saving to: {filename}')
 
@@ -24,13 +34,22 @@ def save(name, journal_data):
     with open(filename, 'w') as fout:
         for entry in journal_data:
             fout.write(entry + '\n')
-        
     #fout.close()
 
+
 def get_full_pathname(name):
+    """
+    This method returns the full path to Journal
+    param name: This is the base name of the journal.
+    """
     filename = os.path.abspath(os.path.join('./journals/', name + '.jrl'))
     return filename
 
 
 def add_entry(text,journal_data):
+    """
+    This method adds an entry to the Journal.
+    param text: This is the text of a new journal entry.
+    param journal_data: is the journal data structure.
+    """
     journal_data.append(text)
