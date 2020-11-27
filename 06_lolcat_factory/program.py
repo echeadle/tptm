78b5535169c40657830397149e381f9f79768c7f
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-27 12:04:08 -0700

    Initial Commit of cat factory.

---
+++ 06_lolcat_factory/program.py
@@ -0,0 +1,28 @@
+"""
+Program to retrive random cat videos
+"""
+
+
+"""
+Main Module, where program starts
+"""
+def main():
+    # Print Header
+    show_header()
+    # get or create output folder
+    # download cat pictures
+    print('Hello from main')
+
+"""
+Simple Show header method
+:params: None
+"""
+def show_header():
+    print('-' * 40)
+    print('           CAT FACTORY')
+    print('-' * 40)
+    print()
+
+
+if __name__ == '__main__':
+    main()
