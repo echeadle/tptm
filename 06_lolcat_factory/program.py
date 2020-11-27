9bcd6bdfdcaf6cfd1d5a05bab2af4b3b04ba6f6d
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-27 12:30:45 -0700

    finished get_or_create_output_folder.

--- 06_lolcat_factory/program.py
+++ 06_lolcat_factory/program.py
@@ -1,27 +1,44 @@
 """
 Program to retrive random cat videos
 """
+import os
 
 
-"""
-Main Module, where program starts
-"""
 def main():
+    """
+    Main Module, where program starts
+    """
     print_header()
     # get or create output folder
+    folder = get_or_create_output_folder()
+    print(f'Found or created folder {folder}')
     # download cat pictures
-    print('Hello from main')
 
-"""
-Simple Show header method
-:params: None
-"""
+
+
 def print_header():
+    """
+    Simple Show header method
+    :params: None
+    """
     print('-' * 40)
     print('           CAT FACTORY')
     print('-' * 40)
     print()
 
+def get_or_create_output_folder():
+    """
+    Get or Create Output folder
+    """
+    base_folder = os.path.dirname(__file__)
+    folder = 'cat_pictures'
+    full_path = os.path.join(base_folder, folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print(f'Createing new directory at {full_path}')
+        os.mkdir(full_path)
+
+    return full_path
 
 if __name__ == '__main__':
     main()
