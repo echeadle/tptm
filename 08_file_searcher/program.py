d955db6ad79b54cb92a710c7a9b676c42e883774
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-28 10:29:31 -0700

    Inital commit of File Search App.

---
+++ 08_file_searcher/program.py
@@ -0,0 +1,31 @@
+
+def main():
+    print_header()
+
+
+def print_header():
+    """
+    Simple Show header method
+    :params: None
+    """
+    print('-' * 40)
+    print('           FILE SEARCH APP')
+    print('-' * 40)
+    print()
+
+
+def get_folder_from_user():
+    pass
+
+
+
+def get_search_text_from_user():
+    pass
+
+
+def search_file():
+    pass
+
+
+if  __name__ == "__main__":
+    main()
