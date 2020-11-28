5cc94c7d03b641902500db7e0c2e7163082521f8
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-28 10:50:57 -0700

    Finished main loop still have search function to write.

--- 08_file_searcher/program.py
+++ 08_file_searcher/program.py
@@ -1,7 +1,17 @@
+import os
 
 def main():
     print_header()
+    folder = get_folder_from_user()
+    if not folder:
+        print("Sorry we can't search that location.")
+        return
+    text = get_search_text_from_user()
+    if not text:
+        print("We can't search for nothing!")
+        return
 
+    search_folders(folder, text)
 
 def print_header():
     """
@@ -15,16 +25,22 @@ def print_header():
 
 
 def get_folder_from_user():
-    pass
+    folder = input('What folder do you want to search? ')
+    if not folder or folder.strip():
+        return None
+    
+    if not os.path.isdir(folder):
+        return None
 
+    return os.path.abspath(folder)
 
 
 def get_search_text_from_user():
-    pass
+    text = input('What are you searching for [single phrases only]?')
+    return text
 
-
-def search_file():
-    pass
+def search_folders(folder, text):
+    print(f'Would search {folder} for {text}.')
 
 
 if  __name__ == "__main__":
