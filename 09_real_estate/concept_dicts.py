79aea0b7b9c6196eeeeb03f2d38c1cf368901b45
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-28 20:51:18 -0700

    Added class to file and showed how dict work with values.

--- 09_real_estate/concept_dicts.py
+++ 09_real_estate/concept_dicts.py
@@ -5,6 +5,15 @@ lookup = dict()
 lookup = {'age': 42, 'loc': 'Italy'}
 lookup = dict(age=42, loc='Italy')
 
+
+class Wizard:
+    def __init__(self, name, level):
+        self.level = level
+        self.name = name
+
+gandolf = Wizard("Gandolf", 42)
+print(gandolf.__dict__)
+
 print(lookup)
 print(lookup['loc'])
 
