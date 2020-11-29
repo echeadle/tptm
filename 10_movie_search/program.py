29f6696cb787bff196ce4c9b01bf206770d77e86
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-29 14:00:27 -0700

    Initial commit of movie search app

---
+++ 10_movie_search/program.py
@@ -0,0 +1,17 @@
+def main():
+    print_header()
+
+
+def print_header():
+    """
+    Simple Show header method
+    :params: None
+    """
+    print('-' * 40)
+    print('        MOVIE SEARCH APP')
+    print('-' * 40)
+    print()
+
+
+if __name__ == '__main__':
+    main()
