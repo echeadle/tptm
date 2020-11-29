0f5aadf44a0d8e791bb315d08e5a8d37844f3844
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-29 06:34:19 -0700

    Added create from dict fucntion

--- 09_real_estate/data_types.py
+++ 09_real_estate/data_types.py
@@ -7,7 +7,7 @@ class Purchase:
         self.latitude  = latitude
         self.price = price
         self.sale_date = sale_date
-        self.home_type = home_type
+        self.type = home_type
         self.sq_ft = sq__ft 
         self.baths = baths 
         self.beds = beds 
@@ -15,3 +15,19 @@ class Purchase:
         self.zip = zip
         self.city = city
         self.street = street 
+
+
+
+def create_from_dict(self, lookup):
+        float(lookup['longitude'])
+        float(lookup['latitude'])
+        float(lookup['price'])
+        lookup['sale_date']
+        lookup['type']
+        int(lookup['sq_ft'])
+        int(lookup['baths'])
+        int(lookup['beds'])
+        lookup['state']
+        lookup['zip']
+        lookup['city']
+        lookup['street']
