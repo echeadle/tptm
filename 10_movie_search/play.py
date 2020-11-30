962dc4bc92584e07b2d0789f208e44a03eb0d64c
Edward Cheadle <edward.cheadle8@gmail.com>
2020-11-29 21:00:22 -0700

    Added list comprehension to read moves.

--- 10_movie_search/play.py
+++ 10_movie_search/play.py
@@ -5,21 +5,48 @@ MovieResult = collections.namedtuple(
     'MovieResult', 
      "imdb_code, title, director, keywords, duration, genres, rating, year, imdb_score")
 
-search = 'capital'
+search = input("What movie do you want to search for? ")
+
 url = 'https://movieservice.talkpython.fm/api/search/{}'.format(search)
 
 resp = requests.get(url)
 resp.raise_for_status()
 
-print(resp.status_code)
+# print(resp.status_code)
 
-print(resp.text)
+# print(resp.text)
 
 movie_data = resp.json()
 movies_list = movie_data.get('hits')
 
 # print(type(movies), movies)
 
-movies = []
-for md in movies_list:
+# movies = []
+# for md in movies_list:
+#     m = MovieResult(
+#         imdb_code = md.get('imdb_code'),
+#         title = md.get('title'),
+#         director = md.get('director'),
+#         keywords = md.get('keywords'),
+#         duration = md.get('duration'),
+#         genres = md.get('genres'),
+#         rating = md.get('rating', 0),
+#         year = md.get('year', 0),
+#         imdb_score = md.get('imdb_score', 0.0)
+#     )
+#     movies.append(m)
+
+# movies = []
+# for md in movies_list:
+#     m = MovieResult(**md)
+#     movies.append(m)
+
+movies = [
+    MovieResult(**md)
+    for md in movies_list
+]
     
+
+print("Found {} movies for search {}".format(len(movies),search))
+for m in movies:
+    print("{} -- {}".format(m.year, m.title))
